8bd01e65c29e3c5debb6965bfefb0d5907d900bd
MennaEssam8 <132103315+MennaEssam8@users.noreply.github.com>
2023-12-01 16:55:54 +0200

    local.js

--- Front End/Yummy/assets/js/local.js
+++ Front End/Yummy/assets/js/local.js
@@ -130,7 +130,7 @@ function ReadAllcards() {
             var cardsHTML = `
                 <div class="col-lg-4 menu-item">
                     <div class="menu-img-container">
-                        <a href="item${record.id}.html"><img src="${record.Photo}" class="menu-img img-fluid" alt="Product Photo"></a>
+                        <img src="${record.Photo}" class="menu-img img-fluid" alt="Product Photo"></a>
                         <div class="info">
                             <h4>${record.Name}</h4>
                             <p class="ingredients">${record.Description}</p>
@@ -158,3 +158,54 @@ ReadAllcards();
    
   
 
+// Function to add an image to the favorites list
+function addToDynamicList(photoSrc) {
+    let favorites = JSON.parse(localStorage.getItem('favorites') || '[]');
+
+    // Add the photo to the favorites list
+    if (!favorites.includes(photoSrc)) {
+        favorites.push(photoSrc);
+        localStorage.setItem('favorites', JSON.stringify(favorites));
+    }
+}
+
+// Function to load and display favorites on a different page
+function loadFavorites() {
+    const photoListDiv = document.getElementById('photoList');
+    photoListDiv.innerHTML = ''; // Clear existing content
+
+    const favorites = JSON.parse(localStorage.getItem('favorites') || '[]');
+
+    favorites.forEach(photoSrc => {
+        const paragraph = document.createElement("p");
+
+        const image = document.createElement('img');
+        image.src = photoSrc;
+        image.style = 'width:300px; height:300px;';
+
+        const removeButton = document.createElement("button");
+        removeButton.innerText = "Remove";
+        removeButton.onclick = function () { removeFromFavorites(photoSrc, paragraph); };
+
+        paragraph.appendChild(image);
+        paragraph.appendChild(removeButton);
+        photoListDiv.appendChild(paragraph);
+    });
+}
+
+// Function to remove an image from favorites
+function removeFromFavorites(photoSrc, paragraphElement) {
+    let favorites = JSON.parse(localStorage.getItem('favorites') || '[]');
+
+    // Find and remove the image from favorites
+    const indexToRemove = favorites.indexOf(photoSrc);
+    if (indexToRemove !== -1) {
+        favorites.splice(indexToRemove, 1);
+        localStorage.setItem('favorites', JSON.stringify(favorites));
+    }
+
+    // Remove the paragraph element from the DOM
+    if (paragraphElement) {
+        paragraphElement.remove();
+    }
+}
